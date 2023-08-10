ab6a006492cce4b215e43fb32726d7b9b10fc018
arvandha121 <nauvan121@gmail.com>
2023-08-10 21:03:24 +0700

    update sql

--- sql/crud-mahasiswa.sql
+++ sql/crud-mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Agu 2023 pada 08.56
+-- Waktu pembuatan: 10 Agu 2023 pada 16.02
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 8.0.2
 
@@ -20,22 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `crud-mahasiswa`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -60,102 +44,17 @@ CREATE TABLE `mahasiswa` (
 --
 
 INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `kota`, `created_at`, `updated_at`) VALUES
-(1, '2041720134', 'Arief Nauvan Ramadha', '2001-11-20', 'Laki-laki', 'Perumahan pondok kencana I/1', 'Nganjuk', '2023-08-02 09:12:15', NULL),
-(2, '2041720130', 'Ahmad Rafif Alaudin', '2002-01-12', 'Laki-laki', 'Perumahan Pondok Asri K/10', 'Kediri', '2023-08-02 09:12:15', NULL),
-(3, '2041720010', 'Taufik Anwar', '2001-08-15', 'Laki-laki', 'Jalan Raya Mulyoagung No.122', 'Malang', '2023-08-02 09:12:15', NULL),
-(4, '2041720098', 'Alun Mega', '2001-03-10', 'Perempuan', 'Jalan Dr. Soetomo V/15', 'Nganjuk', '2023-08-02 09:12:15', NULL),
-(5, '2041720016', 'Atmayanti', '2003-03-15', 'Perempuan', 'Desa Bandung', 'Tulungagung', '2023-08-02 09:12:15', NULL),
-(6, '2041720015', 'Muchammad Rizal Gusnanda Atmaja', '2003-10-12', 'Laki-laki', 'Desa Dawuhan', 'Trenggalek', '2023-08-02 09:12:15', NULL),
-(7, '2041720100', 'Venny Meida Hersianty', '2002-01-11', 'Perempuan', 'Jalan Sidoagung No.155', 'Malang', '2023-08-02 09:12:15', NULL);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(76, '2014_10_12_000000_create_users_table', 1),
-(77, '2014_10_12_100000_create_password_resets_table', 1),
-(78, '2019_08_19_000000_create_failed_jobs_table', 1),
-(79, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(80, '2023_07_24_031620_create_mahasiswa_table', 1);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `personal_access_tokens`
---
-
-CREATE TABLE `personal_access_tokens` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `expires_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `users`
---
-
-INSERT INTO `users` (`id`, `nama`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2y$10$cmk9pEX8uNOs/ZgrOWtAJuVlChORT99IbP8lK7MvTcZVcbwsXqjLG', NULL, NULL, NULL),
-(2, 'nauvan', 'nauvan121@gmail.com', '$2y$10$hqQKOMBKdiI51L8coxiGOexCtzER8OcO0pMjWZhgKtpd1pBvIU1XK', NULL, NULL, NULL);
+(1, '2041720134', 'Arief Nauvan Ramadha', '2001-11-20', 'Laki-laki', 'Kauman', 'Nganjuk', '2023-08-10 07:00:50', NULL),
+(2, '2041720130', 'Ahmad Rafif Alaudin', '2002-01-12', 'Laki-laki', 'Pesantren', 'Kediri', '2023-08-10 07:00:50', NULL),
+(3, '2041720010', 'Taufik Anwar', '2001-08-15', 'Laki-laki', 'Dau', 'Malang', '2023-08-10 07:00:50', NULL),
+(4, '2041720098', 'Alun Mega', '2001-03-10', 'Perempuan', 'Rejoso', 'Nganjuk', '2023-08-10 07:00:50', NULL),
+(5, '2041720016', 'Atmayanti', '2003-03-15', 'Perempuan', 'Ds Bandung', 'Tulungagung', '2023-08-10 07:00:50', NULL),
+(6, '2041720015', 'Muchammad Rizal Gusnanda Atmaja', '2003-10-12', 'Laki-laki', 'Ds Dawuhan', 'Trenggalek', '2023-08-10 07:00:50', NULL),
+(7, '2041720100', 'Venny Meida Hersianty', '2002-01-11', 'Perempuan', 'Lowokwaru', 'Malang', '2023-08-10 07:00:50', NULL);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
 -- Indeks untuk tabel `mahasiswa`
@@ -164,65 +63,14 @@ ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indeks untuk tabel `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
-
---
--- AUTO_INCREMENT untuk tabel `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT untuk tabel `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
-
---
--- AUTO_INCREMENT untuk tabel `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
