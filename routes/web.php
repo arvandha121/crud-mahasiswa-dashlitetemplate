0ff43dc5a72b7d810c0762f7feddc69b07976bec
arvandha121 <nauvan121@gmail.com>
2023-08-10 18:08:36 +0700

    merapikan route

--- routes/web.php
+++ routes/web.php
@@ -19,34 +19,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/laravel', function () {
+    return view('welcome');
+});
 
 Route::redirect('/', '/login');
 Route::get('/login', [SessionController::class, 'index'])->name('login');
 Route::post('/login/check', [SessionController::class, 'login'])->name('login.check');
 
-Route::group(['middleware' => 'App\Http\Middleware\IsLogin'], function () {
+Route::group(['middleware' => ['App\Http\Middleware\IsLogin']], function () {
     Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
-    Route::resource('/admin/data/mahasiswa', MahasiswaController::class);
+    Route::group(['prefix' => 'admin/data/mahasiswa', 'as' => 'mahasiswa.'], function () {
+        Route::get('/', [MahasiswaController::class, 'index'])->name('index');
+        Route::get('/create', [MahasiswaController::class, 'create'])->name('create');
+        Route::post('/', [MahasiswaController::class, 'store'])->name('store');
+        Route::get('/{mahasiswa}/edit', [MahasiswaController::class, 'edit'])->name('edit');
+        Route::put('/{mahasiswa}', [MahasiswaController::class, 'update'])->name('update');
+        Route::delete('/{mahasiswa}', [MahasiswaController::class, 'destroy'])->name('destroy');
+    });
+
     Route::get('/cetak_pdf', [MahasiswaController::class, 'cetak_pdf'])->name('cetak_pdf');
     Route::get('/cetak_excel', [MahasiswaController::class, 'cetak_excel'])->name('cetak_excel');
 
-    Route::get('/admin/data/kota', [KotaController::class, 'index'])->name('kota.index');
-    Route::get('/pdf', [KotaController::class, 'downloadPDF'])->name('kota.downloadPDF');
-    Route::get('/excel', [KotaController::class, 'downloadExcel'])->name('kota.downloadExcel');
+    Route::group(['prefix' => 'admin/data/kota', 'as' => 'kota.'], function () {
+        Route::get('/', [KotaController::class, 'index'])->name('index');
+        Route::get('/pdf', [KotaController::class, 'downloadPDF'])->name('downloadPDF');
+        Route::get('/excel', [KotaController::class, 'downloadExcel'])->name('downloadExcel');
+    });
 
-    Route::resource('/admin/administration', AdministrationController::class);
+    Route::resource('/admin/administration', AdministrationController::class)->except('show');
 
     Route::get('/logout', [SessionController::class, 'logout'])->name('logout');
 });
 
-Route::group([], function () {
-    Route::get('/clear-cache', [CacheClearController::class, 'clearCache']);
-    Route::get('/clear-config', [CacheClearController::class, 'clearConfig']);
-    Route::get('/clear-view', [CacheClearController::class, 'clearView']);
-    Route::get('/clear-route', [CacheClearController::class, 'clearRoute']);
-    Route::get('/clear-optimize', [CacheClearController::class, 'clearOptimize']);
+Route::group(['prefix' => 'clear'], function () {
+    Route::get('cache', [CacheClearController::class, 'clearCache']);
+    Route::get('config', [CacheClearController::class, 'clearConfig']);
+    Route::get('view', [CacheClearController::class, 'clearView']);
+    Route::get('route', [CacheClearController::class, 'clearRoute']);
+    Route::get('optimize', [CacheClearController::class, 'clearOptimize']);
 });
